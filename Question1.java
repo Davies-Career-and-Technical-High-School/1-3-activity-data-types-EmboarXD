671ae8d10666a164fc0212a3de2ebb7164a2c6e0
EmboarXD <31370513+EmboarXD@users.noreply.github.com>
2020-10-02 11:28:44 -0400

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,9 +1,17 @@
 
 
-class Question1 {
-    public static void main(String[] args) {
+import java.util.*;
+import java.lang.reflect.Array;
+//import java.io.*;
 
-        /* Write your code here */
-
+class Main {
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter two doubles: ");
+    String[] doubles = scan.nextLine().split(" ");
+    for (int i = 0; i < 2; i++){
+      Object adouble = Array.get(doubles, 1-i);
+      System.out.print(adouble + " ");
     }
+  }
 }
